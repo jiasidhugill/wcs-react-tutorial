141679f753868440f6cf2f0ff86816052cf9b205
Jia Gill <96542484+jiasidhugill@users.noreply.github.com>
2025-02-13 10:23:28 -0600

    added a page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // add this import statement
+import About from './pages/about'; // import the About page you wrote earlier
+
+function Home() { // move your original return statement into another function
+    return (
+      <div className="App">
+         <h1>This is my web app!</h1>
+         <p>It has a paragraph here</p>
+         <h3>And a subheading here</h3>
+      </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( // add this router element
+    <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import '../styles/about.css';
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Page content</p>
+    </div>
+  );
+}
+
+export default About;
