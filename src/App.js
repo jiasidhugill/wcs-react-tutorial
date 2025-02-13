92d53cab9ca1a0d85aa128594eb858244cb9ae22
Jia Gill <96542484+jiasidhugill@users.noreply.github.com>
2025-02-13 10:28:03 -0600

    finished page routing section of tutorial

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // add this import statement
-import About from './pages/about'; // import the About page you wrote earlier
+// add useNavigate to the import statement
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import About from './pages/about';
 
-function Home() { // move your original return statement into another function
+function Home() {
+    const navigate = useNavigate(); // add this line
+
+    const goToAbout = () => {
+        navigate('/about'); // navigate to the /about page when function is called
+    };
+    
     return (
       <div className="App">
          <h1>This is my web app!</h1>
          <p>It has a paragraph here</p>
          <h3>And a subheading here</h3>
+         <button onClick={goToAbout}>About us</button> {/* this button takes us to the about page*/}
       </div>
     );
 }
 
+// leave the rest of the file unchanged
+
 function App() {
-  return ( // add this router element
+  return (
     <Router>
         <Routes>
             <Route path="/" element={<Home />} />
